d637108fb263a8457aefbe16dd4f6ae6d9c404fb
thawansilva <twnpsilva@gmail.com>
2024-12-17 08:52:08 -0300

    Alteração no css e localização do index.html

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
+// Responsive Navbar
 let navbar_icon = document.getElementById("icon");
 let navbar_list = document.querySelector("ul");
 
 navbar_icon.addEventListener("click", (e) => {
   if (e.target.name === "menu") {
-    console.log("abrir");
     e.target.name = "close";
     navbar_list.classList.add("top-[70px]");
     navbar_list.classList.add("opacity-100");
   } else {
-    console.log("close");
     e.target.name = "menu";
     navbar_list.classList.remove("top-[70px]");
     navbar_list.classList.remove("opacity-100");
   }
 });
 
+// Internal scroll
 let internal_link = document.querySelectorAll("a[href^='#']");
 internal_link.forEach((item) => {
   item.addEventListener("click", (e) => {
@@ -35,3 +35,19 @@ internal_link.forEach((item) => {
     }
   });
 });
+
+// Change on background color
+let onTop = false;
+let navbar = document.querySelector("#nav");
+
+window.onscroll = function (e) {
+  if (window.scrollY < 10 && !onTop) {
+    navbar.classList.remove("onScroll");
+    onTop = true;
+  }
+
+  if (window.scrollY > 10 && onTop) {
+    navbar.classList.add("onScroll");
+    onTop = false;
+  }
+};
